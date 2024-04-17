4e5412f784aa432c752c4d20bc1c18e7cf37750a
Leevi Pohja <124263697+pohjal@users.noreply.github.com>
2024-04-17 13:32:06 +0300

    3.19-3-21

--- index.js
+++ index.js
@@ -28,6 +28,8 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === "CastError") {
     return response.status(400).send({ error: "malformatted id" });
+  } else if (error.name === "ValidationError") {
+    return response.status(400).json({ error: error.message });
   }
 
   next(error);
@@ -84,20 +86,20 @@ app.get("/api/persons", (request, response) => {
 });
 
 app.put("/api/persons/:id", (request, response, next) => {
-  const body = request.body;
-  const person = {
-    name: body.name,
-    number: body.number,
-  };
+  const { name, number } = request.body;
 
-  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+  Person.findByIdAndUpdate(
+    request.params.id,
+    { name, number },
+    { new: true, runValidators: true, context: "query" }
+  )
     .then((updatedPerson) => {
       response.json(updatedPerson);
     })
     .catch((error) => next(error));
 });
 
-app.post("/api/persons", (request, response) => {
+app.post("/api/persons", (request, response, next) => {
   const body = request.body;
   if (!body.name) {
     return response.status(400).json({
@@ -122,9 +124,12 @@ app.post("/api/persons", (request, response) => {
   //     error: "name must be unique",
   //   });
   // }
-  person.save().then((savedPerson) => {
-    response.json(savedPerson);
-  });
+  person
+    .save()
+    .then((savedPerson) => {
+      response.json(savedPerson);
+    })
+    .catch((error) => next(error));
 });
 app.use(errorHandler);
 const PORT = process.env.PORT || 3001;
--- models/person.js
+++ models/person.js
@@ -15,8 +15,23 @@ mongoose
   });
 
 const personSchema = new mongoose.Schema({
-  name: String,
-  number: Number,
+  name: {
+    type: String,
+    minlength: 3,
+    require,
+  },
+  number: {
+    type: String,
+    minlength: 8,
+    require,
+    validate: {
+      validator: function (v) {
+        return /^\d{2,3}-\d{7,8}$/.test(v); //
+      },
+      message: (props) => `${props.value} is not a valid phone number!`,
+    },
+    required: [true, "User phone number required"],
+  },
 });
 
 personSchema.set("toJSON", {
